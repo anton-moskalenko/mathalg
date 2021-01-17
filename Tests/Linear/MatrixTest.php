31f2ad8bb6a338782a2006579e863460b3faea67
Anton Moskalenko <moskalenko.anton@gmail.com>
2021-01-17 13:48:48 +0200

    Update matrix tests.

--- Tests/Linear/MatrixTest.php
+++ Tests/Linear/MatrixTest.php
@@ -12,7 +12,7 @@ class MatrixTest extends TestCase
     /**
      * Test {@link Matrix} for values at correct places.
      */
-    public function testCheck(): void
+    public function testCheckA(): void
     {
         $matrix = new Matrix([[1,2],[3,4]]);
 
@@ -20,5 +20,18 @@ class MatrixTest extends TestCase
         $this->assertEquals(2, $matrix->get(1,2));
         $this->assertEquals(3, $matrix->get(2,1));
         $this->assertEquals(4, $matrix->get(2,2));
+    }
+
+    /**
+     * Test {@link Matrix} for values at correct places.
+     */
+    public function testCheckB(): void
+    {
+        $matrix = new Matrix(2,2,1,2,3);
+
+        $this->assertEquals(1, $matrix->get(1,1));
+        $this->assertEquals(2, $matrix->get(1,2));
+        $this->assertEquals(3, $matrix->get(2,1));
+        $this->assertEquals(0, $matrix->get(2,2));
     }
 }
