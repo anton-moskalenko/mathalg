76e6f7dfcc80a6f96e7e83cb2204962c9afff3ad
Anton Moskalenko <moskalenko.anton@gmail.com>
2021-01-19 15:51:26 +0200

    Add matrix constructor.

--- Source/Linear/Matrix.php
+++ Source/Linear/Matrix.php
@@ -9,5 +9,34 @@ namespace Liloi\MathAlg\Linear;
  */
 class Matrix
 {
+    private $data = [];
 
+    public function __construct(...$parameters)
+    {
+        $first = reset($parameters);
+
+        if(is_array($first))
+        {
+            $this->data = $first;
+            return;
+        }
+
+        if(is_numeric($first))
+        {
+            $delta = 2;
+
+            for($row=1;$row<=$parameters[0];$row++)
+            {
+                for($column=1;$column<=$parameters[1];$column++)
+                {
+                    $this->data[$row-1][$column-1] = $parameters[$delta++] ?? 0;
+                }
+            }
+        }
+    }
+
+    public function get($row, $column)
+    {
+        return $this->data[$row-1][$column-1];
+    }
 }
