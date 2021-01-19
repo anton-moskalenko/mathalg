68c6ad733366783be956510b91247ea0389ddff4
Anton Moskalenko <moskalenko.anton@gmail.com>
2021-01-19 16:46:53 +0200

    MathAlg: add judex exception.

---
+++ Source/MathAlgException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Liloi\MathAlg;
+
+use Judex\ExtendedException;
+
+/**
+ * General algorithms exception.
+ *
+ * @package Exceptions
+ */
+class MathAlgException extends ExtendedException
+{
+    /**
+     * Exception message.
+     *
+     * @var string
+     */
+    protected $defaultMessage = 'General algorithms exception.';
+
+    /**
+     * Exception code.
+     *
+     * @var int|string
+     */
+    protected $defaultCode = 0x101;
+}
